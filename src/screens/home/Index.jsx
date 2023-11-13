298a3348c7fe7933981b9d1045d40d3f3b67ded5
MyFRA <tomyntapss@gmail.com>
2023-11-13 18:07:13 +0700

    feat: basic feature clock-in

--- src/screens/home/Index.jsx
+++ src/screens/home/Index.jsx
@@ -7,6 +7,9 @@ import {
     FlatList,
     StyleSheet,
     BackHandler,
+    TouchableOpacity,
+    PermissionsAndroid,
+    Linking,
 } from 'react-native';
 
 import React, { useState, useEffect } from 'react';
@@ -33,6 +36,15 @@ import Slider from '../../components/Slider';
 import ListPost from '../../components/ListPost';
 import { useIsFocused } from '@react-navigation/native';
 
+import { launchCamera } from 'react-native-image-picker';
+import { ConfirmDialog } from 'react-native-simple-dialogs';
+import { useToast } from 'react-native-toast-notifications';
+import NetInfo from "@react-native-community/netinfo";
+import Geolocation from 'react-native-geolocation-service';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { PERMISSIONS, checkMultiple } from "react-native-permissions";
+import Axios from '../../utils/Axios';
+
 export default function HomeScreen() {
     //init state sliders
     const [loadingSliders, setLoadingSliders] = useState(true);
@@ -42,6 +54,9 @@ export default function HomeScreen() {
     const [loadingPosts, setLoadingPosts] = useState(true);
     const [posts, setPosts] = useState([]);
     const isFocused = useIsFocused()
+    const toast = useToast()
+    const [dialogAskLocation, setDialogAskLocation] = useState(false)
+    const [dialogOpenSetting, setDialogOpenSetting] = useState(false)
 
     //method fetchDataSliders
     const fetchDataSliders = async () => {
@@ -96,8 +111,159 @@ export default function HomeScreen() {
         }
     }, [])
 
+    const requestLocationPermission = async (cb) => {
+        try {
+            const granted = await PermissionsAndroid.request(
+                PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+                {
+                    title: 'Izinkan akses lokasi?',
+                    message: 'Akses lokasi wajib diizinkan, guna diperlukan untuk proses absensi berdasarkan cabang kantor',
+                    buttonNeutral: 'Tanya lagi nanti',
+                    buttonNegative: 'Tolak',
+                    buttonPositive: 'Oke',
+                },
+            );
+            if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+                cb(true, granted)
+            } else {
+                cb(false, granted)
+            }
+        } catch (err) {
+            console.warn(err);
+        }
+    };
+
+    const checkPermissionStatus = (cb) => {
+        checkMultiple([PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION, PERMISSIONS.ANDROID.ACCESS_COARSE_LOCATION]).then(async (res) => {
+            cb(res[PERMISSIONS.ANDROID.ACCESS_COARSE_LOCATION] == 'granted' && res[PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION] == 'granted')
+        })
+    }
+
+    const permissionHandler = (cb) => {
+        checkPermissionStatus((status) => {
+            if (!status) {
+                setDialogAskLocation(true)
+            } else {
+                cb()
+            }
+        })
+    }
+
+    const doClockIn = () => {
+        permissionHandler(() => {
+            doClockInFunc()
+        })
+    }
+
+    const doClockInFunc = async () => {
+        const token = await AsyncStorage.getItem('apiToken')
+
+        NetInfo.fetch().then(state => {
+            if (!state.isConnected) {
+                toast.show('Periksa koneksi internet anda untuk melakukan absensi', {
+                    type: 'danger',
+                    placement: 'center'
+                })
+            } else {
+                Geolocation.getCurrentPosition(
+                    async (position) => {
+
+                        const photoResult = await launchCamera({
+                            mediaType: 'photo',
+                            cameraType: 'front'
+                        })
+
+                        const formData = new FormData();
+
+                        const photo = photoResult.assets[0]
+                        formData.append('photo', {
+                            uri: photo.uri,
+                            type: photo.type,
+                            name: photo.fileName,
+                        })
+                        formData.append('latitude', position.coords.latitude)
+                        formData.append('longitude', position.coords.longitude)
+
+                        Axios.post('/attendances/clock-in', formData, {
+                            headers: {
+                                'Authorization': 'Bearer ' + token,
+                            }
+                        }).then((res) => {
+                            toast.show('Clock In successfully', {
+                                type: 'success',
+                                placement: 'center'
+                            })
+                        }).catch((err) => {
+                            toast.show(err.response.data.error, {
+                                type: 'danger',
+                                placement: 'center'
+                            })
+                        })
+                    })
+            }
+        });
+    }
+
     return (
         <SafeAreaView>
+
+            {/* Confirm Dialog */}
+            <ConfirmDialog
+                title="Akses Lokasi Diperlukan"
+                message="Izinkan aplikasi untuk mengakses lokasi?. Izin ini diperlukan untuk kebutuhan lokasi absensi"
+                visible={dialogOpenSetting}
+                onTouchOutside={() => setDialogOpenSetting(false)}
+                positiveButton={{
+                    title: "YES",
+                    onPress: () => {
+                        setDialogOpenSetting(false)
+                        Linking.openSettings()
+                    }
+                }}
+                negativeButton={{
+                    title: "NO",
+                    onPress: () => {
+                        setDialogOpenSetting(false)
+                    }
+                }}
+            />
+            {/* End of Confirm Dialog */}
+
+            {/* Confirm Dialog */}
+            <ConfirmDialog
+                title="Akses Lokasi Diperlukan"
+                message="Izinkan aplikasi untuk mengakses lokasi?. Izin ini diperlukan untuk kebutuhan lokasi absensi"
+                visible={dialogAskLocation}
+                onTouchOutside={() => setDialogAskLocation(false)}
+                positiveButton={{
+                    title: "YES",
+                    onPress: () => {
+                        setDialogAskLocation(false)
+
+                        requestLocationPermission((statusPermissions, statusName) => {
+                            if (!statusPermissions) {
+                                if (statusName == 'never_ask_again') {
+                                    setDialogOpenSetting(true)
+                                } else {
+                                    toast.show('Tidak bisa melakukan absensi, Harap berikan izin aplikasi untuk mengakses lokasi', {
+                                        type: 'danger',
+                                        placement: 'center'
+                                    })
+                                }
+                            } else {
+                                doClockInFunc()
+                            }
+                        })
+                    }
+                }}
+                negativeButton={{
+                    title: "NO",
+                    onPress: () => {
+                        setDialogAskLocation(false)
+                    }
+                }}
+            />
+            {/* End of Confirm Dialog */}
             {/* header */}
             <View style={styles.header}>
                 <View style={styles.headerContainer}>
@@ -133,12 +299,45 @@ export default function HomeScreen() {
                         />
                     )}
                 </View>
-                <View style={styles.productContainer}>
-                    <Text style={styles.productText}>MAIN MENU</Text>
-                </View>
-
                 {/* Menu */}
+                <View style={styles.productContainer}>
+                    <Text style={[styles.productText, { marginBottom: 10 }]}>MAIN MENU</Text>
+                    <View style={{ gap: 15, flexDirection: 'row' }}>
+                        <TouchableOpacity
+                            onPress={doClockIn}
+                            style={{
+                                shadowColor: '#000',
+                                shadowOffset: {
+                                    width: 0,
+                                    height: 1,
+                                },
+                                shadowOpacity: 0.18,
+                                shadowRadius: 1.0,
+                                flex: 1,
+                                backgroundColor: 'white',
+                                display: 'flex',
+                                alignItems: 'center',
+                                justifyContent: 'center',
+                                paddingVertical: 12,
+                                borderRadius: 5
+                            }}>
+                            <MaterialCommunityIcons
+                                name="location-enter"
+                                style={styles.postIcon}
+                                size={25}
+                            />
+                            <Text style={{ marginTop: 5 }}>Absen Masuk</Text>
+                        </TouchableOpacity>
+                        <View style={{ flex: 1 }}>
+                            <Text>Absen Masuk</Text>
 
+                        </View>
+                        <View style={{ flex: 1 }}>
+                            <Text>Absen Masuk</Text>
+                        </View>
+                    </View>
+                </View>
+                {/* End of Menu */}
                 {/* posts / berita */}
                 <View style={styles.postContainer}>
                     <MaterialCommunityIcons
@@ -215,7 +414,7 @@ const styles = StyleSheet.create({
 
     productContainer: {
         marginTop: 30,
-        flexDirection: 'row',
+        flexDirection: 'column',
     },
 
     productIcon: {
