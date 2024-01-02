86291e98939cb7dc19aa795bfb56b91baad0fca9
muhammad.ramdan <muhammad.ramdan@spesolution.com>
2024-01-02 11:05:08 +0700

    updae image

--- src/screens/splash/Index.jsx
+++ src/screens/splash/Index.jsx
@@ -31,8 +31,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   logo: {
-    width: 200,
-    height: 200,
+    width: 300,
+    height: 300,
     resizeMode: 'contain',
   },
 });
